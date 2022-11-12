d1cc105be1157990b853e34041c14965706c1ad2
chrilleguven <44170953+Chrilleguven@users.noreply.github.com>
2022-11-12 23:12:29 +0100

    removed all un used stuff

--- trust/src/App.js
+++ trust/src/App.js
@@ -1,5 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
--- trust/src/Components/Logo.js
+++ trust/src/Components/Logo.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { NavLink, Link, Router } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Logo = () => {
   return (
     <NavLink to='/'>
-      <a href=''>
-        <div className='logo-container'>
-          <img
-            className='logo'
-            style={{ height: '75px', minWidth: '110px' }}
-            src={require('../img/Ntrust.png')}
-          />
-          <div class='middle'></div>
-        </div>
-      </a>
+      <div className='logo-container'>
+        <img
+          className='logo'
+          style={{ height: '75px', minWidth: '110px' }}
+          src={require('../img/Ntrust.png')}
+          alt=''
+        />
+        <div class='middle'></div>
+      </div>
     </NavLink>
   );
 };
