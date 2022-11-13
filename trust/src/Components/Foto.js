c7f5a29e5f3a0c1e7bb03ef9c5bb4a0b63b07e95
chrilleguven <44170953+Chrilleguven@users.noreply.github.com>
2022-11-13 18:33:03 +0100

    removed prototyp map problem

--- trust/src/Components/Foto.js
+++ trust/src/Components/Foto.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Foto = () => {
   function importAll(r) {
     const images = [];
-    r.keys().map((item, index) => {
+    r.keys().forEach((item, index) => {
       images[item.replace('./', '')] = r(item);
     });
     return images;
